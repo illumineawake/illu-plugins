50556d674a8126624d0875dbec9ee84975bda443
illumineawake <illumineawake@gmail.com>
2020-05-22 18:38:29 +1000

    test: adjust gradle dependency

--- test/test.gradle.kts
+++ test/test.gradle.kts
@@ -39,7 +39,7 @@ tasks {
                     "Plugin-Version" to project.version,
                     "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
                     "Plugin-Provider" to project.extra["PluginProvider"],
-                    "Plugin-Dependencies" to nameToId("BotUtils"),
+                    "Plugin-Dependencies" to nameToId("botutils"),
                     "Plugin-Description" to project.extra["PluginDescription"],
                     "Plugin-License" to project.extra["PluginLicense"]
             ))
