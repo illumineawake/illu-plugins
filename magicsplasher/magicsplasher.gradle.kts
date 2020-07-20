f38a568a479ad54ab101f86359aa50246051b784
illumineawake <illumineawake@gmail.com>
2020-07-20 18:07:10 +1000

    bootstrap

--- magicsplasher/magicsplasher.gradle.kts
+++ magicsplasher/magicsplasher.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.9.1"
+version = "1.9.2"
 
 project.extra["PluginName"] = "Magic Splasher"
 project.extra["PluginDescription"] = "Illumine automated magic splasher"
--- smokerunecrafter/smokerunecrafter.gradle.kts
+++ smokerunecrafter/smokerunecrafter.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "1.2.0"
+version = "1.2.1"
 
 project.extra["PluginName"] = "Smoke Runecrafter Plugin"
 project.extra["PluginDescription"] = "Illumine - Smoke Runecrafting plugin"
