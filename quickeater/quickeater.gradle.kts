a8f279a5d65a246aa16ed5973eda37092b90dcf7
illumineawake <illumineawake@gmail.com>
2020-08-10 00:05:09 +1000

    quickeater: supports combat potions

--- quickeater/quickeater.gradle.kts
+++ quickeater/quickeater.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "2.3.0"
+version = "2.4.0"
 
 project.extra["PluginName"] = "Quick Eater"
 project.extra["PluginDescription"] = "Illumine - auto eat food below configured HP"
