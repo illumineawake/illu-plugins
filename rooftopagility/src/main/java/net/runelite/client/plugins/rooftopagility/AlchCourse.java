db7f0eb4211da7fd3daf3d7fef05951fae3d09e8
Shieldeh <josburr@gmail.com>
2020-10-02 20:37:43 +1000

    Config cleanup, dropdowns show supported courses.

---
+++ rooftopagility/src/main/java/net/runelite/client/plugins/rooftopagility/AlchCourse.java
@@ -0,0 +1,20 @@
+package net.runelite.client.plugins.rooftopagility;
+
+import lombok.Getter;
+
+@Getter
+public enum AlchCourse {
+    DRAYNOR("Draynor Village"),
+    VARROCK("Varrock"),
+    CANAFIS("Canafis"),
+    FALADOR("Falador"),
+    SEERS("Seers Village"),
+    ARDOUGNE("Ardougne");
+
+    private final String name;
+
+    AlchCourse(String name)
+    {
+        this.name = name;
+    }
+}
--- rooftopagility/src/main/java/net/runelite/client/plugins/rooftopagility/RooftopAgilityPlugin.java
+++ rooftopagility/src/main/java/net/runelite/client/plugins/rooftopagility/RooftopAgilityPlugin.java
@@ -234,7 +234,7 @@ public class RooftopAgilityPlugin extends Plugin {
     }
 
     private boolean shouldCastTeleport() {
-        return config.camelotTeleport() && client.getBoostedSkillLevel(Skill.MAGIC) >= 45 &&
+        return (config.camelotTeleport() || config.camelotTeleportAlch()) && client.getBoostedSkillLevel(Skill.MAGIC) >= 45 &&
                 CAMELOT_TELE_LOC.distanceTo(client.getLocalPlayer().getWorldLocation()) <= 3 &&
                 (utils.inventoryContains(ItemID.LAW_RUNE) && utils.inventoryContainsStack(ItemID.AIR_RUNE, 5) ||
                         utils.inventoryContains(ItemID.LAW_RUNE) && utils.isItemEquipped(AIR_STAFFS));
