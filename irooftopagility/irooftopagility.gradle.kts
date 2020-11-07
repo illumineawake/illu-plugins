6d69fe4991e3f776dbac9f24c8d6057d6248f065
illumineawake <illumineawake@gmail.com>
2020-11-07 16:24:27 +1100

    irooftopagility: 2nd obstacle (tree) bug fix - untested

--- irooftopagility/irooftopagility.gradle.kts
+++ irooftopagility/irooftopagility.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "5.0.2"
+version = "5.0.3"
 
 project.extra["PluginName"] = "iRooftop Agility"
 project.extra["PluginDescription"] = "Illumine automated rooftop agility plugin"
