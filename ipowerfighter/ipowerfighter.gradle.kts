8869d2231d140b2a7571f4074b96f48c1af1c134
Justin <github@justin.pm>
2021-01-15 03:47:18 +1100

    iPowerFighter: added rune pouch check to alch process

--- ipowerfighter/ipowerfighter.gradle.kts
+++ ipowerfighter/ipowerfighter.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "3.4.4"
+version = "3.4.5"
 
 project.extra["PluginName"] = "iPowerfighter Plugin"
 project.extra["PluginDescription"] = "Illumine - Powerfighter plugin"
