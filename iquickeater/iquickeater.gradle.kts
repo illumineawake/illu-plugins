3f7241debdc9fd699910d4b7d06d2d4b705f6fbf
illumineawake <illumineawake@gmail.com>
2021-05-14 23:01:18 +1000

    bootstrap iquickeater: add option for eat at HP % iutils: adds Zanaris banks

--- iquickeater/iquickeater.gradle.kts
+++ iquickeater/iquickeater.gradle.kts
@@ -24,7 +24,7 @@
  */
 
 
-version = "5.1.1"
+version = "5.1.2"
 
 project.extra["PluginName"] = "iQuick Eater"
 project.extra["PluginDescription"] = "Illumine - auto eat food, consume potions and equip items"
