5ffd621be8748b228b2424d4f2b5099681422660
illumineawake <illumineawake@gmail.com>
2021-09-03 16:13:00 +1000

    bootstrap RealityWinner amendments

--- iutils/iutils.gradle.kts
+++ iutils/iutils.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "4.5.4"
+version = "4.5.5"
 
 project.extra["PluginName"] = "iUtils"
 project.extra["PluginDescription"] = "Illumine - Utils required for plugins to function with added automation"
