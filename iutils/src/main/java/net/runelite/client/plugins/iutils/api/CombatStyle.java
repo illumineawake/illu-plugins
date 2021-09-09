dec2a6e99b45ac4a024de60c9ae07fe949c6812d
illumineawake <illumineawake@gmail.com>
2021-09-09 19:59:54 +1000

    iutils: add skill to CombatStyle

--- iutils/src/main/java/net/runelite/client/plugins/iutils/api/CombatStyle.java
+++ iutils/src/main/java/net/runelite/client/plugins/iutils/api/CombatStyle.java
@@ -2,16 +2,18 @@ package net.runelite.client.plugins.iutils.api;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import net.runelite.api.Skill;
 
 @Getter
 @AllArgsConstructor
 public enum CombatStyle {
-    ACCURATE(CombatType.MELEE),
-    AGGRESSIVE(CombatType.MELEE),
-    SPECIAL(CombatType.MELEE),
-    DEFENSIVE(CombatType.MELEE),
-    RAPID(CombatType.RANGED),
-    MAGIC(CombatType.MAGIC);
+    ACCURATE(CombatType.MELEE, Skill.ATTACK),
+    AGGRESSIVE(CombatType.MELEE, Skill.STRENGTH),
+    SPECIAL(CombatType.MELEE, Skill.STRENGTH),
+    DEFENSIVE(CombatType.MELEE, Skill.DEFENCE),
+    RAPID(CombatType.RANGED, Skill.RANGED),
+    MAGIC(CombatType.MAGIC, Skill.MAGIC);
 
     private final CombatType combatType;
+    private final Skill skill;
 }
