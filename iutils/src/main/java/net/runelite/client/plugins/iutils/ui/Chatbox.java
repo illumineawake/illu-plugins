12c21ab9fbd2dbb53890bb641dff62e7a56c247e
illumineawake <illumineawake@gmail.com>
2022-02-11 22:34:55 +1100

    iutils: fix chat dialogue continue widgets

--- iutils/src/main/java/net/runelite/client/plugins/iutils/ui/Chatbox.java
+++ iutils/src/main/java/net/runelite/client/plugins/iutils/ui/Chatbox.java
@@ -101,10 +101,10 @@ public class Chatbox {
             case OPTIONS_CHAT:
                 throw new IllegalStateException("can't continue, this is an options chat");
             case PLAYER_CHAT:
-                game.widget(217, 4).select();
+                game.widget(217, 5).select();
                 break;
             case NPC_CHAT:
-                game.widget(231, 4).select();
+                game.widget(231, 5).select();
                 break;
             case ITEM_CHAT:
                 game.widget(11, 4).select();
