ac7c0a5aa207094d2d941f590161cb20be8477d7
illumineawake <illumineawake@gmail.com>
2022-03-01 19:33:41 +1100

    update gradle version

--- iworldwalker/iworldwalker.gradle.kts
+++ iworldwalker/iworldwalker.gradle.kts
@@ -23,7 +23,7 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-version = "4.0.7"
+version = "4.0.8"
 
 project.extra["PluginName"] = "iWorld Walker Plugin"
 project.extra["PluginDescription"] = "Illumine - World Walker plugin"
